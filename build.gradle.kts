268a56a893b4c3f56f0161ba5fa1f2d4dbaf7613
Accelerator <129496126+VectorTransformation@users.noreply.github.com>
2025-11-03 14:32:22 +0900

    fix load command

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 group = "eternalScript"
-val pluginVersion = "1.0.3"
+val pluginVersion = "1.0.4"
 val javaVersion = 21
 val pluginApiVersion = "1.21.8"
 val minecraftVersion = "1.21.10"
